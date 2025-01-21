30a088c68a5041d74b2b6237c4b4d3eadf4980ce
Simon Sekavcnik <simon.sekavcnik@gmail.com>
2025-01-21 12:42:09 +0100

    Created the protobufs for the api

---
+++ README.md
@@ -0,0 +1,3 @@
+# QuReedGUIServer
+
+This small utility is used to establish communication between the project Python runtime and the GUI runtime.
---
+++ setup.py
@@ -0,0 +1,21 @@
+"""qureed installation configuration"""
+
+import os
+
+from setuptools import find_packages, setup
+
+current_dir = os.path.abspath(os.path.dirname(__file__))
+setup(
+    name="qureed_project_server",
+    version="0.0.1",
+    author="Simon Sekavčnik",
+    author_email="simon.sekavcnik@tum.de",
+    description="QuReed Grafical Interface",
+    license="Apache 2.0",
+    packages=find_packages(where="."),
+    install_requires=[
+        "grpcio",
+    ],
+    entry_points={
+    },
+)
